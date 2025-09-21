382024715e83d987a853799cd0843a0307fdd4d4
obulanaidu <obulnaiduganaparty@gmail.com>
2025-09-21 21:45:21 +0530

    terraform

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@
  output "public_subnet_ids" {
    value = aws_subnet.public[*].id  # 8means gives all public ids
 }
+
+ output "private_subnet_ids" {
+   value = aws_subnet.private[*].id  # 8means gives all public ids
+}
+
+ output "database_subnet_ids" {
+   value = aws_subnet.database[*].id  # 8means gives all public ids
+}
