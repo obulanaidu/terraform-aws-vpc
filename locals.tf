5af27d3b818afb913da00d14a533c24a32f19c2b
obulanaidu <obulnaiduganaparty@gmail.com>
2025-09-21 16:24:52 +0530

    terraform

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
     Terraform = "true"
     }
 
-    public_subnet_cidrs= ["10.0.1.0/24", "10.0.2.0/24"]
+    #public_subnet_cidrs= ["10.0.1.0/24", "10.0.2.0/24"]
 
     az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 }
